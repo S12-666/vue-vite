01a8cd84ea07729fd24341803bcf8d199424d6ed
WenjuLiu <lius90613@gmail.com>
2026-01-04 20:08:13 +0800

    增加轧制力和长度变化的图

---
+++ src/views/dataoverview/rolling/LengthCurve.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, onMounted, onBeforeUnmount, watch, shallowRef, nextTick } from 'vue';
+import * as echarts from 'echarts';
+
+const props = defineProps({
+    curveData: {
+        type: Object,
+        default: () => ({ position: [], force: [], torque: [] })
+    }
+});
+
+</script>
+
+<template>
+    <div ref="chartRef" style="height: 100%; width: 100%"></div>
+</template>
