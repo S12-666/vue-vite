74a9c176da7ce8b904e2f9b78a8977705645e10b
WenjuLiu <lius90613@gmail.com>
2026-01-05 21:02:19 +0800

    增加厚度变化的echarts

---
+++ src/views/dataoverview/rolling/ThickCurve.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted, onBeforeUnmount, watch, shallowRef, nextTick } from 'vue';
+import * as echarts from 'echarts';
+
+const props = defineProps({
+    curveData: {
+        type: Object,
+        default: () => ({ position: [], force: [], torque: [] })
+    }
+});
+
+</script>
+
+<template>
+    <div ref="chartRef" style="height: 100%; width: 100%"></div>
+</template>
+
+<style scoped></style>
